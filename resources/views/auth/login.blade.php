0f6c30277cf713db389dfa066df45ccb42bb0395
anibal-carmona <anibal.carmona@outlook.com>
2021-11-06 11:37:42 -0300

    Se agrega vista login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>Login</title>
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/img/favicons/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/img/favicons/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/img/favicons/favicon-16x16.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/favicons/favicon.ico')}}">
+    <link rel="manifest" href="{{asset('assets/img/favicons/manifest.json')}}">
+    <meta name="msapplication-TileImage" content="{{asset('assets/img/favicons/mstile-150x150.png')}}">
+    <meta name="theme-color" content="#ffffff">
+    <script src="{{asset('assets/js/config.js')}}"></script>
+    <script src="{{asset('vendors/overlayscrollbars/OverlayScrollbars.min.js')}}"></script>
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap"
+        rel="stylesheet">
+    <link href="{{asset('vendors/overlayscrollbars/OverlayScrollbars.min.css')}}" rel="stylesheet">
+    {{-- <link href="{{asset('assets/css/theme-rtl.min.css')}}" rel="stylesheet" id="style-rtl"> --}}
+    <link href="{{asset('assets/css/theme.min.css')}}" rel="stylesheet" id="style-default">
+    {{-- <link href="{{asset('assets/css/user-rtl.min.css')}}" rel="stylesheet" id="user-style-rtl"> --}}
+    <link href="{{asset('assets/css/user.min.css')}}" rel="stylesheet" id="user-style-default">
+
+</head>
+
+<body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+
+    <main class="main" id="top">
+        <div class="container-fluid">
+
+            <div class="row min-vh-100 bg-100">
+                <div class="col-6 d-none d-lg-block position-relative">
+                    <div class="bg-holder"
+                        style="background-image:url({{asset('assets/img/generic/17.jpg')}}); background-position: 50% 50%;">
+                    </div>
+                    <!--/.bg-holder-->
+                </div>
+                
+                <div class="col-sm-10 col-md-6 px-sm-0 align-self-center mx-auto py-5">
+                    <div class="row justify-content-center g-0">
+                        <div class="col-lg-9 col-xl-8 col-xxl-6">
+                            <div class="card">
+                                <div class="card-header bg-circle-shape bg-shape text-center p-2">
+                                    <a class="font-sans-serif fw-bold fs-4 z-index-1 position-relative link-light light" href="#">
+                                        Portafolio
+                                    </a>
+                                </div>
+                                <div class="card-body p-4">
+
+                                    <div class="row flex-between-center">
+                                        <div class="col-auto">
+                                            <h3>Login</h3>
+                                        </div>
+                                    </div>
+
+                                    @include('auth.form')
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="{{asset('vendors/popper/popper.min.js')}}"></script>
+    <script src="{{asset('vendors/bootstrap/bootstrap.min.js')}}"></script>
+    <script src="{{asset('vendors/anchorjs/anchor.min.js')}}"></script>
+    <script src="{{asset('vendors/is/is.min.js')}}"></script>
+    <script src="{{asset('vendors/fontawesome/all.min.js')}}"></script>
+    <script src="{{asset('vendors/lodash/lodash.min.js')}}"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="{{asset('vendors/list.js/list.min.js')}}"></script>
+    <script src="{{asset('assets/js/theme.js')}}"></script>
+
+</body>
