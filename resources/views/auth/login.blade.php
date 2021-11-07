bd5f92114d3a735a02bb65e4d154b3357bacbc3e
anibal-carmona <anibal.carmona@outlook.com>
2021-11-07 14:42:54 -0300

    se crea vista Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,15 +28,13 @@
     <!--    Stylesheets-->
     <!-- ===============================================-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
     <link href="{{asset('vendors/overlayscrollbars/OverlayScrollbars.min.css')}}" rel="stylesheet">
     {{-- <link href="{{asset('assets/css/theme-rtl.min.css')}}" rel="stylesheet" id="style-rtl"> --}}
     <link href="{{asset('assets/css/theme.min.css')}}" rel="stylesheet" id="style-default">
     {{-- <link href="{{asset('assets/css/user-rtl.min.css')}}" rel="stylesheet" id="user-style-rtl"> --}}
     <link href="{{asset('assets/css/user.min.css')}}" rel="stylesheet" id="user-style-default">
-
+    
 </head>
 
 <body>
