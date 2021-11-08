d137971d1b0dbf73ef9c38617b07750edee39ad7
anibal-carmona <anibal.carmona@outlook.com>
2021-11-07 22:19:07 -0300

    Se corrige vista Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
-    
-    <div class="card">
 
-        {{ Route::currentRouteName() }}
+<div class="card mb-3">
 
+    <div class="bg-holder d-none d-lg-block bg-card"
+        style="background-image:url(../../assets/img/icons/spot-illustrations/corner-2.png);">
     </div>
+    <!--/.bg-holder-->
+
+    <div class="card-body position-relative">
+        <div class="row">
+            <div class="col-lg-8">
+                <h3>Dashboard</h3>
+                <p class="mb-0">Aplicación de prueba</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
